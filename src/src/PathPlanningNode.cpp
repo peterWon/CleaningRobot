15b3230b1e0b1ee927b4187b8995cc4d6d0aa3c7
zhangjunhui <zhangjunhui@cvte.com>
2020-11-25 09:46:39 +0800

    update melodic devel

--- src/src/PathPlanningNode.cpp
+++ src/src/PathPlanningNode.cpp
@@ -1,21 +1,21 @@
 #include "CleaningPathPlanner.h"
 #include <boost/shared_ptr.hpp>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <tf2_ros/buffer.h>
 
-namespace cm = costmap_2d;
-namespace rm = geometry_msgs;
 
 using std::vector;
-using rm::PoseStamped;
 using std::string;
-using cm::Costmap2D;
-using cm::Costmap2DROS;
+
+using geometry_msgs::PoseStamped;
+using costmap_2d::Costmap2D;
+using costmap_2d::Costmap2DROS;
 
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "path_planning_node");
 
-    tf::TransformListener tf(ros::Duration(10));
+    tf2_ros::Buffer tf(ros::Duration(10));
 
     costmap_2d::Costmap2DROS lcr("cleaning_costmap", tf);
     //planner_costmap_ros_->pause();
